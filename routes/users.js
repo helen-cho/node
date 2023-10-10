89c87370c8e75484bb1f2167465e9d937a818e60
helen-cho <hd9536@daum.net>
2023-10-10 16:27:27 +0900

    ver01

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title:'사용자페이지', pageName:'users/users.ejs'})
+router.get('/', function (req, res, next) {
+    var sql='select * from users';
+    db.get().query(sql, function(err, rows){
+        res.send(rows);
+    });
 });
 
 module.exports = router;
