231261d05a66af9c17f57006a27f2f1876e67764
helen-cho <hd9536@daum.net>
2024-04-25 18:05:05 +0900

    게시글정보

--- ex04/routes/posts.js
+++ ex04/routes/posts.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res, next) {
 
 //게시판 목록 데이터 불러오기
 router.get('/list.json', function(req, res){
-    const sql='select * from posts order by pid desc';
+    const sql='select *,date_format(pdate, "%Y-%m-%d %T") fdate from posts order by pid desc';
     db.get().query(sql, function(err, rows){
         if(err){
             console.log('게시판목록:', err);
@@ -32,8 +32,27 @@ router.post('/insert', function(req, res){
     console.log(title, contents, uid);
     const sql="insert into posts(title,contents,writer) values(?,?,?)";
     db.get().query(sql, [title, contents, uid], function(err, rows){
+        if(err){
+            console.log('글쓰기 오류:', err);
+        }
         res.redirect('/posts');
     });
+});
+
+//게시글 Read 페이지이동
+router.get('/read', function(req, res){
+    const pid=req.query.pid;
+    console.log(pid);
+    const sql="select *,date_format(pdate, '%Y-%m-%d %T') fdate from posts where pid=?";
+    db.get().query(sql, [pid], function(err, rows){
+        console.log(rows[0]);
+        res.render('index.ejs', 
+            {
+                title:'게시글정보', 
+                pageName:'posts/read.ejs',
+                post:rows[0]
+            });
+    })
 });
 
 module.exports = router;
