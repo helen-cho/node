a7dcdbfe5ce5e18a40efd839846f5a90ae9c4f5a
helen-cho <hd9536@daum.net>
2024-05-28 18:16:57 +0900

    사진업로드

--- shop/routes/users.js
+++ shop/routes/users.js
@@ -1,6 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var multer = require('multer');
+
+var upload = multer({
+  storage:multer.diskStorage({
+    destination:(req, file, done)=>{
+      done(null, './public/photos')
+    },
+    filename:(req, file, done)=>{
+      var filename=Date.now() + '.jpg';
+      done(null, filename);
+    }
+  })
+});
+
+//사진업로드
+router.post('/photo', upload.single('file'), function(req, res){
+  const uid=req.body.uid;
+  const fileName='/photos/' + req.file.filename;
+  const sql='update users set photo=? where uid=?';
+  db.get().query(sql, [fileName, uid], function(err, rows){
+    if(err){
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -24,7 +51,7 @@ router.get('/read/:uid', function(req, res){
   });
 });
 
-//로그인
+//로그인체크
 router.post('/login', function(req, res){
   const uid=req.body.uid;
   const upass=req.body.upass;
@@ -50,6 +77,23 @@ router.post('/insert', function(req, res){
   const uname=req.body.uname;
   const sql='insert into users(uid,upass,uname) values(?,?,?)';
   db.get().query(sql, [uid, upass, uname], function(err, rows){
+    if(err){
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
+
+//회원정보수정
+router.post('/update', function(req, res){
+  const uid=req.body.uid;
+  const uname=req.body.uname;
+  const address1=req.body.address1;
+  const address2=req.body.address2;
+  const phone=req.body.phone;
+  const sql='update users set uname=?,address1=?,address2=?,phone=? where uid=?';
+  db.get().query(sql,[uname,address1,address2,phone,uid], function(err, rows){
     if(err){
       res.send({result:0});
     }else{
