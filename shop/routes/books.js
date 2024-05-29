6599b1c4c1c63285d5ec5c77c3aaa8e524819ca1
helen-cho <hd9536@daum.net>
2024-05-29 13:13:14 +0900

    도서등록

--- shop/routes/books.js
+++ shop/routes/books.js
@@ -11,12 +11,20 @@ router.post('/insert', function(req, res, next) {
   const publisher=req.body.publisher;
   const author=req.body.authors;
   const image=req.body.thumbnail;
-  console.log(title,price,contents,isbn,publisher,author,image);
+  //console.log(title,price,contents,isbn,publisher,author,image);
 
   const sql='select * from books where isbn=?';
   db.get().query(sql, [isbn], function(err, rows){
-    if(!rows){
-      res.send({result:1});
+    if(rows.length==0){
+      const sql1='insert into books(title,price,contents,isbn,publisher,author,image) values(?,?,?,?,?,?,?)';
+      db.get().query(sql1, [title,price,contents,isbn,publisher,author,image], function(err, rows){
+        if(err){
+          console.log('err.............', err);
+          res.send({result:0})
+        }else {
+          res.send({result:1});
+        }
+      })
     }else{
       res.send({result:0});
     }
