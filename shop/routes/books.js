728084f69f21e85c3b2c504639bde3b86c98a885
helen-cho <hd9536@daum.net>
2024-05-29 16:59:48 +0900

    도서목록

--- shop/routes/books.js
+++ shop/routes/books.js
@@ -12,23 +12,37 @@ router.post('/insert', function(req, res, next) {
   const author=req.body.authors;
   const image=req.body.thumbnail;
   //console.log(title,price,contents,isbn,publisher,author,image);
-
+  console.log('.............', author);
   const sql='select * from books where isbn=?';
   db.get().query(sql, [isbn], function(err, rows){
     if(rows.length==0){
       const sql1='insert into books(title,price,contents,isbn,publisher,author,image) values(?,?,?,?,?,?,?)';
       db.get().query(sql1, [title,price,contents,isbn,publisher,author,image], function(err, rows){
-        if(err){
-          console.log('err.............', err);
-          res.send({result:0})
-        }else {
-          res.send({result:1});
-        }
+        res.send({result:1});
       })
     }else{
       res.send({result:0});
     }
   });  
+});
+
+//도서목록 테스트:/books/list?page=1&size=5
+router.get('/list', function(req, res){
+  const page=parseInt(req.query.page);
+  const size=parseInt(req.query.size);
+  let sql ="select *,date_format(regdate,'%Y-%m-%d') fmtdate,format(price,0) fmtprice";
+      sql+=" from books ";
+      sql+=" order by bid desc";
+      sql+=" limit ?,?";
+
+  db.get().query(sql, [(page-1)*size, size], function(err, rows){
+    const documents = rows;
+    sql = "select count(*) total from books";
+    db.get().query(sql, function(err, rows){
+      const count=rows[0].total;
+      res.send({count, documents});
+    })
+  });
 });
 
 module.exports = router;
