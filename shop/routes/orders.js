242fcdd1ae069522ab618a8ee41e9e4c74d03ad2
helen-cho <hd9536@daum.net>
2024-06-03 18:31:06 +0900

    주문하기

--- shop/routes/orders.js
+++ shop/routes/orders.js
@@ -26,4 +26,21 @@ router.post('/purchase', function(req, res){
   });
 })
 
+//주문상품입력
+router.post('/insert', function(req, res){
+  const pid=req.body.pid;
+  const bid=req.body.bid;
+  const price=req.body.price;
+  const qnt=req.body.qnt;
+  const sql="insert into orders(pid,bid,price,qnt) values(?,?,?,?)";
+  db.get().query(sql, [pid,bid,price,qnt], function(err, rows){
+    if(err){
+      console.log('...........err', err);
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
+
 module.exports = router;
