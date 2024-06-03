dcc1c6783df48101c82c31b893ae0615c136286b
helen-cho <hd9536@daum.net>
2024-06-03 13:16:05 +0900

    장바구니

--- shop/routes/cart.js
+++ shop/routes/cart.js
@@ -14,6 +14,45 @@ router.post('/insert', function(req, res){
   const sql='insert into cart(uid, bid) values(?,?)';
   db.get().query(sql, [uid, bid], function(err, rows){
     if(err){
+      console.log('err..............', err);
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
+
+//장바구니목록
+router.get('/list/', function(req, res){
+  const uid=req.query.uid;
+  const sql="select * from view_cart where uid=? order by regDate desc";
+  db.get().query(sql,[uid], function(err, rows){
+    res.send(rows);
+  })
+});
+
+//수량수정
+router.post('/update', function(req, res){
+  const uid=req.body.uid;
+  const bid=req.body.bid;
+  const qnt=req.body.qnt;
+  const sql="update cart set qnt=? where bid=? and uid=?";
+  db.get().query(sql, [qnt, bid, uid], function(err, rows){
+    if(err){
+      res.send({reslut:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
+
+//장바구니삭제
+router.post('/delete', function(req, res){
+  const uid=req.body.uid;
+  const bid=req.body.bid;
+  const sql="delete from cart where uid=? and bid=?";
+  db.get().query(sql, [uid, bid], function(err, rows){
+    if(err){
       res.send({result:0});
     }else{
       res.send({result:1});
